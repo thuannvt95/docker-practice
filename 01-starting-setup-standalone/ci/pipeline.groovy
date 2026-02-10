6b78c0ab71c26f2e1fcbe362f4badce46d54d86e
thuan_nvt <>
2026-02-10 13:42:55 +0700

    add

--- 01-starting-setup-standalone/ci/pipeline.groovy
+++ 01-starting-setup-standalone/ci/pipeline.groovy
@@ -13,7 +13,7 @@ pipeline {
         IMAGE_NAME = "angular-docker-node-multibranch"
         CONTAINER_NAME = "angular-docker-node-multibranch"
         APP_PORT = "90"
-        CONTAINER_PORT = "5000"
+        CONTAINER_PORT = "6000"
     }
 
     stages {
